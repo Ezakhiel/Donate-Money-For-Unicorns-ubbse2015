7018b1cd5aaade2a097ca7ff7c8609da0754a0b1
Hajna Turáni <Hajna Turni>
2016-01-15 13:12:11 +0200

    coop in progress(60%)

--- Dynamic Games/Dynamic Games/CoopForm.Designer.cs
+++ Dynamic Games/Dynamic Games/CoopForm.Designer.cs
@@ -28,10 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CoopForm));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.chartProfit = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.numericPlayer = new System.Windows.Forms.NumericUpDown();
             this.richTextBoxMaterials = new System.Windows.Forms.RichTextBox();
@@ -44,11 +46,11 @@
             this.label4 = new System.Windows.Forms.Label();
             this.richTextBoxWinning = new System.Windows.Forms.RichTextBox();
             this.dgvCoalition = new System.Windows.Forms.DataGridView();
-            this.PlayerNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Coalitions = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.buttonClear = new System.Windows.Forms.Button();
             this.buttonStop = new System.Windows.Forms.Button();
             this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Coalitions = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.chartProfit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericPlayer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCoalition)).BeginInit();
@@ -64,8 +66,11 @@
             this.chartProfit.Location = new System.Drawing.Point(12, 261);
             this.chartProfit.Name = "chartProfit";
             series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.White;
             series1.Legend = "Legend1";
-            series1.Name = "Series1";
+            series1.Name = "Coalitions";
+            series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.YValuesPerPoint = 3;
             this.chartProfit.Series.Add(series1);
             this.chartProfit.Size = new System.Drawing.Size(385, 230);
             this.chartProfit.TabIndex = 0;
@@ -74,24 +79,30 @@
             // numericPlayer
             // 
             this.numericPlayer.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.numericPlayer.Location = new System.Drawing.Point(500, 49);
+            this.numericPlayer.Location = new System.Drawing.Point(645, 63);
             this.numericPlayer.Name = "numericPlayer";
             this.numericPlayer.Size = new System.Drawing.Size(144, 20);
             this.numericPlayer.TabIndex = 1;
+            this.numericPlayer.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
             // 
             // richTextBoxMaterials
             // 
             this.richTextBoxMaterials.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.richTextBoxMaterials.Location = new System.Drawing.Point(457, 160);
+            this.richTextBoxMaterials.Location = new System.Drawing.Point(602, 174);
             this.richTextBoxMaterials.Name = "richTextBoxMaterials";
             this.richTextBoxMaterials.Size = new System.Drawing.Size(229, 111);
             this.richTextBoxMaterials.TabIndex = 4;
-            this.richTextBoxMaterials.Text = "";
+            this.richTextBoxMaterials.Text = "1 1 0 3\n0 0 0 2\n2 2 2 2";
+            this.richTextBoxMaterials.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBoxMaterials_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(533, 33);
+            this.label1.Location = new System.Drawing.Point(678, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 13);
             this.label1.TabIndex = 7;
@@ -100,7 +111,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(464, 92);
+            this.label2.Location = new System.Drawing.Point(609, 106);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 8;
@@ -109,7 +120,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(611, 92);
+            this.label3.Location = new System.Drawing.Point(756, 106);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 9;
@@ -118,7 +129,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(548, 144);
+            this.label5.Location = new System.Drawing.Point(693, 158);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(49, 13);
             this.label5.TabIndex = 11;
@@ -128,7 +139,7 @@
             // 
             this.buttonNewPlayer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.buttonNewPlayer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonNewPlayer.Location = new System.Drawing.Point(456, 108);
+            this.buttonNewPlayer.Location = new System.Drawing.Point(601, 122);
             this.buttonNewPlayer.Name = "buttonNewPlayer";
             this.buttonNewPlayer.Size = new System.Drawing.Size(108, 23);
             this.buttonNewPlayer.TabIndex = 13;
@@ -139,7 +150,7 @@
             // 
             this.buttonLeaver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.buttonLeaver.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonLeaver.Location = new System.Drawing.Point(581, 108);
+            this.buttonLeaver.Location = new System.Drawing.Point(726, 122);
             this.buttonLeaver.Name = "buttonLeaver";
             this.buttonLeaver.Size = new System.Drawing.Size(108, 23);
             this.buttonLeaver.TabIndex = 14;
@@ -149,20 +160,21 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(524, 281);
+            this.label4.Location = new System.Drawing.Point(669, 295);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.Size = new System.Drawing.Size(84, 13);
             this.label4.TabIndex = 17;
-            this.label4.Text = "Player\'s winning ratio";
+            this.label4.Text = "Player\'s function";
             // 
             // richTextBoxWinning
             // 
             this.richTextBoxWinning.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.richTextBoxWinning.Location = new System.Drawing.Point(457, 297);
+            this.richTextBoxWinning.Location = new System.Drawing.Point(602, 311);
             this.richTextBoxWinning.Name = "richTextBoxWinning";
             this.richTextBoxWinning.Size = new System.Drawing.Size(229, 111);
             this.richTextBoxWinning.TabIndex = 16;
-            this.richTextBoxWinning.Text = "";
+            this.richTextBoxWinning.Text = "X1+X2+X3\nX2+X3\n2*X4";
+            this.richTextBoxWinning.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBoxWinning_KeyPress);
             // 
             // dgvCoalition
             // 
@@ -175,7 +187,6 @@
             this.dgvCoalition.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
             this.dgvCoalition.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCoalition.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.PlayerNo,
             this.Coalitions});
             this.dgvCoalition.Cursor = System.Windows.Forms.Cursors.Hand;
             this.dgvCoalition.GridColor = System.Drawing.SystemColors.Window;
@@ -190,32 +201,15 @@
             this.dgvCoalition.RowTemplate.DefaultCellStyle.NullValue = null;
             this.dgvCoalition.RowTemplate.ReadOnly = true;
             this.dgvCoalition.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvCoalition.Size = new System.Drawing.Size(385, 230);
+            this.dgvCoalition.Size = new System.Drawing.Size(544, 230);
             this.dgvCoalition.TabIndex = 15;
             this.dgvCoalition.SelectionChanged += new System.EventHandler(this.dataGridViewCoalition_SelectionChanged);
-            // 
-            // PlayerNo
-            // 
-            this.PlayerNo.FillWeight = 50F;
-            this.PlayerNo.Frozen = true;
-            this.PlayerNo.HeaderText = "Player";
-            this.PlayerNo.Name = "PlayerNo";
-            this.PlayerNo.ReadOnly = true;
-            this.PlayerNo.Width = 50;
-            // 
-            // Coalitions
-            // 
-            this.Coalitions.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Coalitions.HeaderText = "Coalitions";
-            this.Coalitions.Name = "Coalitions";
-            this.Coalitions.ReadOnly = true;
-            this.Coalitions.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             // 
             // buttonClear
             // 
             this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.buttonClear.Location = new System.Drawing.Point(518, 463);
+            this.buttonClear.Location = new System.Drawing.Point(672, 457);
             this.buttonClear.Name = "buttonClear";
             this.buttonClear.Size = new System.Drawing.Size(108, 34);
             this.buttonClear.TabIndex = 18;
@@ -229,7 +223,7 @@
             this.buttonStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.buttonStop.Image = global::Dynamic_Games.Properties.Resources.stop;
-            this.buttonStop.Location = new System.Drawing.Point(578, 414);
+            this.buttonStop.Location = new System.Drawing.Point(448, 378);
             this.buttonStop.Name = "buttonStop";
             this.buttonStop.Size = new System.Drawing.Size(108, 34);
             this.buttonStop.TabIndex = 12;
@@ -243,7 +237,7 @@
             this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.buttonStart.Image = ((System.Drawing.Image)(resources.GetObject("buttonStart.Image")));
-            this.buttonStart.Location = new System.Drawing.Point(457, 414);
+            this.buttonStart.Location = new System.Drawing.Point(448, 311);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(108, 34);
             this.buttonStart.TabIndex = 6;
@@ -252,12 +246,30 @@
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Coalitions
+            // 
+            this.Coalitions.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Coalitions.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Coalitions.FillWeight = 300F;
+            this.Coalitions.HeaderText = "Coalitions";
+            this.Coalitions.MinimumWidth = 542;
+            this.Coalitions.Name = "Coalitions";
+            this.Coalitions.ReadOnly = true;
+            this.Coalitions.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Coalitions.Width = 542;
+            // 
             // CoopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(734, 502);
+            this.ClientSize = new System.Drawing.Size(878, 502);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.richTextBoxWinning);
@@ -301,8 +313,8 @@
         private System.Windows.Forms.Label label4;
         public System.Windows.Forms.RichTextBox richTextBoxWinning;
         private System.Windows.Forms.DataGridView dgvCoalition;
-        private System.Windows.Forms.DataGridViewTextBoxColumn PlayerNo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Coalitions;
         private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Coalitions;
     }
 }
--- Dynamic Games/Dynamic Games/CoopForm.cs
+++ Dynamic Games/Dynamic Games/CoopForm.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CColor = System.Drawing.Color;
+using System.Resources;
 
 namespace Dynamic_Games
 {
     public partial class CoopForm : Form
     {
         Random randomGen = new Random();
+        bool isClicked = false;
 
         //close the form
         protected override void OnFormClosing(FormClosingEventArgs e)
@@ -21,39 +23,31 @@ namespace Dynamic_Games
             base.OnFormClosing(e);
 
             if (e.CloseReason == CloseReason.WindowsShutDown) return;
-
-            //// Confirm user wants to close
-            //switch (MessageBox.Show(this, "Are you sure you want to close?", "Closing", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
-            //{
-            //    case DialogResult.No:
-            //        e.Cancel = true;
-            //        break;
-            //    default:
-            //        this.DialogResult = DialogResult.OK;
-            //        break;
-            //}
         }
 
         public CoopForm()
         {
             InitializeComponent();
             initDGV();
+            initInput();
         }
 
         private void initDGV()
         {
             dgvCoalition.AutoGenerateColumns = false;
-            dgvCoalition.ColumnCount = 2;
-            dgvCoalition.Columns[0].Name = "Player";
-            dgvCoalition.Columns[1].Name = "Coalitions";
+            dgvCoalition.ColumnCount = 1;
+            //dgvCoalition.Columns[0].Name = "Players";
+            dgvCoalition.Columns[0].Name = "Coalitions";
         }
 
         private void initInput()
         {
             buttonClear.Enabled = false;
-            numericPlayer.Value = 0;
-            richTextBoxMaterials.Text = "";
-            richTextBoxWinning.Text = "";
+            numericPlayer.Value = 3;
+            var text = "1 1 0 3" + Environment.NewLine + "0 0 0 2" + Environment.NewLine + "2 2 2 2";
+            richTextBoxMaterials.Text = text;
+            text = "X1+X2+X3" + Environment.NewLine + "X2+X3" + Environment.NewLine + "2*X4";
+            richTextBoxWinning.Text = text;
         }
 
         //check if everything is filled.
@@ -66,6 +60,7 @@ namespace Dynamic_Games
             return true;
         }
 
+        //start the magic function xD
         private void buttonStart_Click(object sender, EventArgs e)
         {
             if (CheckFill())
@@ -76,7 +71,15 @@ namespace Dynamic_Games
                     buttonStart.Image = Dynamic_Games.Properties.Resources.pause;
                     buttonClear.Enabled = false;
                     buttonStop.Enabled = true;
+
+                    //var winningTokenized = tokenizer(richTextBoxWinning.Text);
+                    var materialsTokenized = tokenizer(richTextBoxMaterials.Text);
+
+                    DgvFirstRow();
                     FillDataGrid();
+
+                    isClicked = false;
+                    timer1.Enabled = true;
                 }
                 else if (buttonStart.Text == "   Pause")
                 {
@@ -84,6 +87,8 @@ namespace Dynamic_Games
                     buttonStart.Image = Dynamic_Games.Properties.Resources.play;
                     buttonClear.Enabled = true;
                     buttonStop.Enabled = true;
+                    isClicked = true;
+                    timer1.Enabled = false;
                 }
                 else
                 {
@@ -91,6 +96,8 @@ namespace Dynamic_Games
                     buttonStart.Image = Dynamic_Games.Properties.Resources.pause;
                     buttonClear.Enabled = false;
                     buttonStop.Enabled = true;
+                    isClicked = false;
+                    timer1.Enabled = true;
                 }
             }
             else
@@ -98,44 +105,172 @@ namespace Dynamic_Games
                 MessageBox.Show(this, "You have to fill everything!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
         //stop button
         private void buttonStop_Click(object sender, EventArgs e)
         {
-            buttonStart.Text = "Start";
+            buttonStart.Text = "   Start";
             buttonStart.Enabled = false;
             buttonClear.Enabled = true;
             buttonStop.Enabled = false;
+            isClicked = true;
+            timer1.Enabled = false;
         }
 
         //clear button
         private void buttonClear_Click(object sender, EventArgs e)
         {
             dgvCoalition.Rows.Clear();
+            dgvCoalition.Refresh();
+
             initInput();
             buttonStart.Enabled = true;
+            buttonStart.Image = Dynamic_Games.Properties.Resources.play;
             buttonStop.Enabled = false;
             buttonClear.Enabled = false;
-        }
-
-        private CColor GetRandomColor()
-        {
-            return CColor.FromArgb(randomGen.Next(0, 255), randomGen.Next(0, 255), randomGen.Next(0, 255));
-            // The error is here
-        }
-
-        // fill the datagrid with players and coalition
-        public void FillDataGrid()
-        {
-            for (int i = 0; i < numericPlayer.Value; i++)
+            foreach (var series in chartProfit.Series)
             {
-                dgvCoalition.Rows.Add("P" + (i + 1));
-                dgvCoalition.Rows[i].Cells[0].Style.ForeColor = GetRandomColor();
+                series.Points.Clear();
+            }
+            
+        }
+
+        private int[] tokenizer(string inputString)
+        {
+            //string[] splitString = inputString.Split(' ').ToArray();
+            try
+            {
+                string[] stringSeparators = new string[] { " ", "\n", "\t" };
+                int[] splitInt = inputString.Split(stringSeparators, StringSplitOptions.None).Select(word => Int32.Parse(word)).ToArray();
+                return splitInt;
+            }
+            catch
+            {
+                Console.WriteLine("Tokenizer failed! Check input");
+                throw;
+            }
+        }
+
+        //private CColor GetRandomColor()
+        //{
+        //    return CColor.FromArgb(randomGen.Next(0, 255), randomGen.Next(0, 255), randomGen.Next(0, 255));
+        //}
+
+        // fill dgv's first row with players
+        private void DgvFirstRow()
+        {
+            var s = GetPlayersArray(Decimal.ToInt32(numericPlayer.Value));
+            var list = "";
+            foreach (var l in s)
+            {
+                list += "(" + l + ") ";
+            }
+            dgvCoalition.Rows.Add(list);
+            dgvCoalition.DefaultCellStyle.Font = new Font(Font, FontStyle.Bold);
+        }
+
+        // fill the datagrid with coalition
+        private void FillDataGrid()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                var coal = BackTrack(Decimal.ToInt32(numericPlayer.Value));
+                var c = "";
+                foreach (var e in coal)
+                {
+                    c += e;
+                }
+                var row = new object[] { c };
+                dgvCoalition.Rows.Add(row);
+                //dgvCoalition.Rows[i].Cells[0].Style.ForeColor = GetRandomColor();
                 dgvCoalition.DefaultCellStyle.Font = new Font(Font, FontStyle.Bold);
             }
+        }
 
-            for (int i = 0; i < numericPlayer.Value; i++)
+        private void chart()
+        {
+           // for(int i = 0; i < )
+        }
+
+        private List<string> GetPlayersArray(int numberOfPlayers)
+        {
+            try
             {
+                List<string> res = new List<string>();
+                for (int i = 0; i < numberOfPlayers; i++)
+                {
+                    res.Add("P" + (i + 1));
+                }
+                return res;
+            }
+            catch
+            {
+                Console.WriteLine("Couldn't create the players list!");
+                return new List<string>();
+            }
+        }
 
+        // from players list, make random coalitions, output ex.: (P1,P2)   (P3)
+        private List<string> BackTrack(int numOfPlayers)
+        {
+            try
+            {
+                const string SPACE = ")    ";
+                List<string> getList = new List<string>();
+                List<string> res = new List<string>();
+                getList = GetPlayersArray(numOfPlayers);
+                int rnd, playersCount = getList.Count;
+
+                while (playersCount > 0)
+                {
+                    rnd = randomGen.Next(0, playersCount + 1);
+                    if (rnd >= playersCount)
+                    {
+                        if (res.Count != 0 && res.Last() != SPACE)
+                        {
+                            res.Add(SPACE);
+                        }
+                    }
+                    else
+                    {
+                        var toadd = getList[rnd];
+                        if (res.Count == 0 || res.Last() == SPACE)
+                        {
+                            if (getList.Count == 1)
+                            {
+                                res.Add("(" + toadd + ")");
+                            }
+                            else
+                            {
+                                res.Add("(" + toadd);
+                            }
+                        }
+                        else if (getList.Count == 1)
+                        {
+                            if (res.Last() != SPACE)
+                            {
+                                res.Add("," + toadd + ")");
+                            }
+                            else
+                            {
+                                res.Add(toadd + ")");
+                            }
+                        }
+                        else
+                        {
+                            res.Add("," + toadd);
+                        }
+                        getList.RemoveAt(rnd);
+                        playersCount--;
+                    }
+                }
+                return res;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Couldn't create the coalitions list!");
+                return new List<string>();
             }
         }
 
@@ -144,39 +279,34 @@ namespace Dynamic_Games
             dgvCoalition.ClearSelection();
         }
 
+        private void richTextBoxMaterials_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetterOrDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar);
+        }
 
+        private void richTextBoxWinning_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            // Check for a naughty character in the KeyDown event.
+            if (System.Text.RegularExpressions.Regex.IsMatch(e.KeyChar.ToString(), @"[Xx0-9+\-\/\*\^]"))
+            {
+                // Stop the character from being entered into the control since it is illegal.
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
 
-        //Random randomGen = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //dgvCoalition.Rows.Clear();
+            ///dgvCoalition.Refresh();
+            //int addPlayers = Decimal.ToInt32(numericPlayer.Value) + 1;
+            //int leftPlayers = Decimal.ToInt32(numericPlayer.Value) - 1;
 
-        //private CColor GetRandomColor()
-        //{
-        //    return CColor.FromArgb(randomGen.Next(0, 255), randomGen.Next(0, 255), randomGen.Next(0, 255));
-        //    // The error is here
-        //}
-
-        //private void dataGridViewCoalition_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
-        //{
-        //    //KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
-        //    //KnownColor randomColorName = names[randomGen.Next(names.Length)];
-            
-
-        //    for (int i = 0; i < numericPlayer.Value; i++)
-        //    {
-        //        SolidBrush Brs = new SolidBrush(GetRandomColor());
-        //        if (e.ColumnIndex == 1 && e.RowIndex > -1)
-        //        {
-        //            GraphicsExtensions.FillCircle(e.Graphics, Brs, e.CellBounds.Location.X + 18, e.CellBounds.Location.Y + 10, 7);
-        //            e.Handled = true;
-        //        }
-        //    }
-        //}
+            FillDataGrid();
+        }
     }
-
-    //public static class GraphicsExtensions
-    //{
-    //    public static void FillCircle(this Graphics g, Brush brush, float centerX, float centerY, float radius)
-    //    {
-    //        g.FillEllipse(brush, centerX - radius, centerY - radius, radius + radius, radius + radius);
-    //    }
-    //}
 }
